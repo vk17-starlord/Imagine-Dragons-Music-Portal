1032561a44ff16db8ba736f35ef1ff13eebf8f34
vk17-starlord <khollam.vinit@gmail.com>
2021-03-03 22:57:41 +0530

    added proj

---
+++ index.js
@@ -0,0 +1,17 @@
+// initialize aos
+AOS.init();
+
+var swiper = new Swiper('.swiper-container', {
+    effect: 'cube',
+    grabCursor: true,
+    cubeEffect: {
+      shadow: true,
+      slideShadows: true,
+      shadowOffset: 20,
+      shadowScale: 0.94,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
+
